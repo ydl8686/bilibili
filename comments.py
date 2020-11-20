20c8d9a5e9451111d9e53081413be2d931ef4b4d
181250178@smail.nju.edu.cn <181250178@smail.nju.edu.cn>
2020-11-20 12:24:15 +0800

    final

--- comments.py
+++ comments.py
@@ -1,13 +1,12 @@
 import jieba
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import requests
 import re
 from lxml import etree
 import pandas as pd
-from wordcloud import random_color_func
 
-r=requests.get('https://search.bilibili.com/all?keyword=黑人抬棺&from_source=nav_suggest_new')
+r=requests.get('https://search.bilibili.com/all?keyword=网抑云&from_source=nav_suggest_new')
 print("----------------------------------------")
 content = r.text
 result = re.findall('<a href="//www.bilibili.com/video/(.*?)?from=search"', content, re.S)
@@ -34,22 +33,3 @@ for i in cidList:
     # 存到本地
     # 解决了中文乱码问题
     dm_df.to_csv('/Users/yindelin/Desktop/comments.csv', encoding='utf_8_sig', mode='a', header=False)
-# jieba分词
-dm_str = " ".join(tmp)
-words_list = jieba.lcut(dm_str)  # 切分的是字符串,返回的是列表
-words_str = " ".join(words_list)
-# 读取本地文件
-# backgroud_Image = plt.imread('/Users/yindelin/Documents/个人信息/图片/柴犬.jpeg')
-
-# 创建词云
-wc = WordCloud(
-    background_color='white',
-    font_path='/System/Library/Fonts/PingFang.ttc',  # 设置本地字体
-
-    max_words=2000,
-    max_font_size=100,
-    min_font_size=10,
-)
-
-word_cloud = wc.generate(words_str) # 产生词云
-word_cloud.to_file("/Users/yindelin/Desktop/yumu.jpg") #保存图片
